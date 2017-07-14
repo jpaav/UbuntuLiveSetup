d797e02d7024e4f8dbf2aa0451d33e9e08a31a26
Kronium <kronium@ubuntu.(none)>
2017-07-14 19:49:57 +0000

    initial commit

---
+++ SetupLiveLinux.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
+sudo apt-get update
+sudo apt-get install ia32-libs
+sudo apt-get install linux-headers-generic
