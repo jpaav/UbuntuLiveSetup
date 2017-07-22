1c0cf59db0b3fe13cfab410b82addbaad91e80ac
jpaav <joepaavola@gmail.com>
2017-07-22 21:04:22 +0000

    Update SetupLiveLinux.sh
    added webhelper fix

--- SetupLiveLinux.sh
+++ SetupLiveLinux.sh
@@ -1,7 +1,13 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install build-essential
+#
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 sudo apt-get update
 sudo apt-get install ia32-libs
 sudo apt-get install linux-headers-generic
+#
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
