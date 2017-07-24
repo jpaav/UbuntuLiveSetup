cfda193a1a1e2edd711f67120ad6a7fc881c3dec
jpaav <joepaavola@gmail.com>
2017-07-24 01:44:32 +0000

    Streamlined apt-get updates/upgrades

--- SetupLiveLinux.sh
+++ SetupLiveLinux.sh
@@ -1,13 +1,12 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install build-essential
-#
+# install nvidia drivers here (?)
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
-sudo apt-get update
-sudo apt-get install ia32-libs
-sudo apt-get install linux-headers-generic
-#
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get dist-upgrade
+sudo apt-get install ia32-libs
+sudo apt-get install linux-headers-generic
+
